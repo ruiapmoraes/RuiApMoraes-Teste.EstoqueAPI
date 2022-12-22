b8f9ffa7507b9e31e576ee67fab358ec60da2245
Rui Moraes <rui-moraes@live.com>
2022-12-22 00:01:39 -0300

    Efetuando a implementação dos serviços.

---
+++ RuiMoraes.Teste.EstoqueAPI/Services/Interfaces/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using RuiMoraes.Teste.EstoqueAPI.Data.Interfaces;
+using RuiMoraes.Teste.EstoqueAPI.Domains;
+using RuiMoraes.Teste.EstoqueAPI.DTOs;
+
+namespace RuiMoraes.Teste.EstoqueAPI.Services.Interfaces
+{
+    public interface IGenericService<TEntity> where TEntity : BaseEntidade
+    {
+        IEnumerable<TEntity> BuscarTodos();
+        TEntity BuscarPorId(int? id);
+        void Criae(TEntity categoryDto);
+        void Editar(TEntity categoryDto);
+      
+
+
+    }
+}
---
+++ RuiMoraes.Teste.EstoqueAPI/Services/Interfaces/IProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using RuiMoraes.Teste.EstoqueAPI.DTOs;
+
+namespace RuiMoraes.Teste.EstoqueAPI.Services.Interfaces
+{
+    public interface IProdutoService
+    {
+        IEnumerable<ProdutoDTO> BuscarTodos();
+        ProdutoDTO BuscarPorId(int? id);
+        void Criar(ProdutoDTO produtoDTO);
+
+        void Edtar(ProdutoDTO productDto);
+    }
+}
